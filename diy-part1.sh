33323be29786d3616919cdc7714195d9dfe8a431
ywlc <522805227@qq.com>
2022-09-16 14:00:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,7 +29,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # 添加插件源码
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # passwall依赖
